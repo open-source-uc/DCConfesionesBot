d8e8bea0ba02ac195ccd5f974114e757ce2cd424
Hernan4444 <hfvaldivieso@uc.cl>
2017-11-09 15:06:19 -0300

    Version con template

--- flask_heroku.py
+++ flask_heroku.py
@@ -2,8 +2,9 @@
 import flask
 import json
 import requests
+import os
 
-TOKEN = 'CENSURADO POR SEGURIDAD'
+TOKEN = os.environ["token"]
 
 app = flask.Flask(__name__)
 
@@ -15,7 +16,8 @@ def id_generator():
         n += 1
 
 messages = {}
-template_admin = "**Nuevo Mensaje\tid:** {}\n{}"
+template_admin = "*Nuevo Mensaje\tid:* {}\n{}"
+template_public = "*DCConfesión #{} * \n{}"
 
 message_id = id_generator()
 
@@ -32,12 +34,18 @@ def send_message(texto, id_, markdown=False):
 
     return requests.get(url, params=(params))
 
-admin_group = -313969292
-public_gruop = -1001331104046
+admin_group = int(os.environ["admin_group"])
+public_gruop = int(os.environ["public_gruop"])
+channel = int(os.environ["channel"])
+
+send_message("*Me han reiniciado :(*", admin_group, True)
+
+tag_message = 1
 
 
 @app.route('/Bot', methods=["POST", "GET"])
 def telegram_bot():
+    global tag_message
 
     try:
         if "edited_message" not in json.loads(flask.request.data):
@@ -65,27 +73,47 @@ def telegram_bot():
                         send_message(messages[id_], admin_group)
 
                 elif text.lower().startswith("/all"):
-                    text = ", ".join([str(x) for x in messages.keys()])
-                    send_message(text, admin_group)
+                    if len(messages.keys()) == 0:
+                        send_message("No hay mensajes pendientes", admin_group)
+                    else:
+                        text = ", ".join([str(x) for x in messages.keys()])
+                        send_message(text, admin_group)
 
-                elif text.lower().startswith("/response"):
-                    text = text.replace("/response", "")
-                    send_message("Bot: " + text, public_gruop)
+                elif text.lower().startswith("/set"):
+                    new_id = int(text.replace("/set ", ""))
+                    tag_message = new_id
+                    send_message("ID de los mensajes seteado en {}".format(tag_message), admin_group)
+
+                elif text.lower().startswith("/r"):
+                    text = text.replace("/r ", "")
+                    send_message("*Admin: *" + text, public_gruop, True)
+                    send_message("*Admin: *" + text, channel, True)
 
                 elif text.lower().startswith("/yes"):
                     try:
                         command, id_ = text.strip().split(" ")
                         id_ = int(id_)
                         if id_ in messages:
-                            send_message(messages[id_],
+                            send_message(template_public.format(tag_message, messages[id_]),
                                          public_gruop,
+                                         True)
+                            send_message(template_public.format(tag_message, messages[id_]),
+                                         channel,
                                          True)
 
                             del messages[id_]
+                            tag_message += 1
+
                     except ValueError:
                         send_message("No se pudo procesar la respuesta",
                                      admin_group,
                                      True)
+                elif text.lower().startswith("/no all"):
+                    messages.clear()
+                    send_message("Mensajes eliminados",
+                                 admin_group,
+                                 True)
+
                 elif text.lower().startswith("/no"):
                     try:
                         command, id_ = text.strip().split(" ")
@@ -100,7 +128,11 @@ def telegram_bot():
                                      admin_group,
                                      True)
             return "None"
+
+        return "None"
+
     except Exception as e:
+        print("ERROR EN EL BOT\n", e)
         # Si es que se genera un error que no deja aceptar más mensajes
         return "None"
 
